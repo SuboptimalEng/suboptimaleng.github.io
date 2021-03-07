150e891a59c03694c8fd54f146316772113f85b2
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-03-07 13:20:14 -0500

    formatting.

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,7 +3,9 @@
     class="grid grid-cols-1 divide-y divide-gray-200 font-sans antialiased max-w-screen-md mx-auto p-4"
   >
     <!-- topbar -->
-    <div class="flex flex-row justify-between pb-4 font-bold text-3xl sm:text-5xl">
+    <div
+      class="flex flex-row justify-between pb-4 font-bold text-3xl sm:text-5xl"
+    >
       <div>
         <g-link to="/" class="hover:underline">
           <!-- {{ $static.metadata.siteName }} -->
@@ -22,7 +24,11 @@
 
     <!-- footer -->
     <div class="flex flex-row pt-4 justify-center">
-      <div v-for="social in socials" :key="social.href" class="mx-1 text-3xl sm:text-5xl">
+      <div
+        v-for="social in socials"
+        :key="social.href"
+        class="mx-1 text-3xl sm:text-5xl"
+      >
         <a :href="social.href">
           <font-awesome-icon :icon="social.icon" />
         </a>
