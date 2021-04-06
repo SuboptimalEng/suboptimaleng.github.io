a45491adf3ed0a7a7173291e7dec3000986e43c5
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-04-05 20:35:09 -0500

    add pic.

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,9 +11,9 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'A blog of a typical Indian guy who makes videos about web development and productivity.' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
   },
 
   googleAnalytics: {
