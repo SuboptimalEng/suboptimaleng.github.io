bbcf5288a0df4035656ee5ca38937d56f9c41b0e
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-04-06 12:45:39 -0500

    update tag page title.

--- pages/tag/_slug.vue
+++ pages/tag/_slug.vue
@@ -11,28 +11,27 @@ export default {
     Articles,
   },
 
-  head() {
-    return {
-      title: 'Tag Search: ' + this.article[0].title + ' - Suboptimal Engineer',
-      meta: [
-        ...getMetaTags({
-          title:
-            'Tag Search: ' + this.article[0].title + ' - Suboptimal Engineer',
-          description: DEFAULT_DESCRIPTION,
-          image: 'https://suboptimaleng.github.io/suboptimal.png',
-          url: 'suboptimaleng.github.io',
-        }),
-      ],
-    };
-  },
-
   async asyncData({ $content, params }) {
     const articles = (
       await $content('articles').sortBy('createdAt', 'desc').fetch()
     ).filter((article) => {
       return article.tags.includes(params.slug);
     });
-    return { articles };
+    return { articles, tag: params.slug };
+  },
+
+  head() {
+    return {
+      title: 'Tag Search: ' + this.tag + ' - Suboptimal Engineer',
+      meta: [
+        ...getMetaTags({
+          title: 'Tag Search: ' + this.tag + ' - Suboptimal Engineer',
+          description: DEFAULT_DESCRIPTION,
+          image: 'https://suboptimaleng.github.io/suboptimal.png',
+          url: 'suboptimaleng.github.io',
+        }),
+      ],
+    };
   },
 };
 </script>
