2b25d10a3dc7cd1d42386dcd69332399cc51935f
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-08-09 19:32:48 -0500

    Meta tags work now

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,12 +48,12 @@ export default function Home() {
       <Head>
         <title>Suboptimal Engineer</title>
         <meta
-          property="image"
+          property="og:image"
           content="https://suboptimaleng.vercel.app/channel_art.png"
         />
-        <meta property="title" content="Suboptimal Engineer" />
+        <meta property="og:title" content="Suboptimal Engineer" />
         <meta
-          property="description"
+          property="og:description"
           // content="The typical blog of an atypical Indian guy who quit his tech job to become a YouTuber."
           // content="Just a typical Indian software engineer building products for creators and teaching web dev on YouTube."
           content="👨🏾‍💻 Indian software engineer + YouTuber building products to help creators."
