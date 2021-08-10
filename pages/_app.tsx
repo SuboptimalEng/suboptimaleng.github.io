2ee2d9d10d19014186b4b7d9df655dc1e8b37007
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-08-10 18:50:25 -0500

    Add google analytics.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/dist/client/router';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const handleRouteChange = (url: URL) => {
+    window.gtag('config', 'UA-191446508-2', {
+      page_path: url,
+    });
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
