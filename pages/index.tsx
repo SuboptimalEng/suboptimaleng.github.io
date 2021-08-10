1cad5f3e0ce0c527414abf58577a8427e442819a
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-08-09 19:21:58 -0500

    Update meta tags.

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,7 +50,7 @@ export default function Home() {
         <meta property="og:image" content="/image.png" />
         <meta property="og:title" content="Suboptimal Engineer" />
         <meta
-          name="og:description"
+          property="og:description"
           // content="The typical blog of an atypical Indian guy who quit his tech job to become a YouTuber."
           // content="Just a typical Indian software engineer building products for creators and teaching web dev on YouTube."
           content="👨🏾‍💻 Indian software engineer + YouTuber building products to help creators."
