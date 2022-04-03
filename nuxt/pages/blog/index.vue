e6966b5ec812b48a0b9af5821459963cbf92673f
suboptimaleng <suboptimaleng@gmail.com>
2022-04-03 14:00:56 -0500

    Remove old blog.

--- nuxt/pages/blog/index.vue
+++
@@ -1,7 +0,0 @@
-<script>
-export default {
-  middleware({ redirect }) {
-    return redirect('301', '/blog/page/1');
-  },
-};
-</script>
--- nuxt/pages/tag/index.vue
+++
@@ -1,7 +0,0 @@
-<script>
-export default {
-  middleware({ redirect }) {
-    return redirect('301', '/blog/2021-01-about');
-  },
-};
-</script>
