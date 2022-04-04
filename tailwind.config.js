cc48c91ca7ba7570769aa6592dbd3b35882ea00b
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-04-04 14:27:14 -0500

    No components folder.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: [
     './_next/**/*.{html,js,ts,jsx,tsx}',
     './pages/**/*.{html,js,ts,jsx,tsx}',
-    './components/**/*.{html,js,ts,jsx,tsx}',
+    // './components/**/*.{html,js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
