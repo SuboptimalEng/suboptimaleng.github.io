0532f7e7e7ce758f9f8aa7e9562c9f0e248cde68
SuboptimalEng <70355008+SuboptimalEng@users.noreply.github.com>
2022-04-04 18:30:54 +0000

    Deploying to gh-pages from @ SuboptimalEng/suboptimaleng.github.io@88be5225f37f2d0d2e80ddbee6bd599635836c7c 🚀

--- _next/static/8UEDNiZ1N6MYGioqRz74p/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/a9a7754c-ed85a2e5ff29d6fb.js","static/chunks/1-8873b7e6b24bd661.js","static/chunks/pages/index-bb8d0bcc45478be1.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
