9e28749f5894fb418218728840a5a85faf50ffd9
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-07-08 16:13:49 -0500

    Remove comment.

--- pages/snake/index.tsx
+++ pages/snake/index.tsx
@@ -10,7 +10,6 @@ function SnakeGame() {
     const test = new SceneInit('myThreeJsCanvas');
     const snake = new Snake();
     test.scene.add(snake.group);
-    console.log('remembering how git works lol.');
 
     const update = () => {
       test.update();
