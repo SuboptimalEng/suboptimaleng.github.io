8503dc7502cc08ed5e96734d618dc2f92bcab561
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-07-28 15:13:43 -0500

    Hide shader code for now.

--- lib/snake/Board.ts
+++ lib/snake/Board.ts
@@ -68,63 +68,55 @@ class Board {
 
   _createIndividualPortal(coord: IPosition) {
     const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-    const boxMaterial = new THREE.ShaderMaterial({
-      uniforms: this.uniforms,
-      vertexShader: `
-        uniform float u_time;
-
-        varying vec2 vUv;
-
-        void main() {
-          vUv = uv;
-
-          gl_Position = projectionMatrix
-            * modelViewMatrix
-            * vec4(position.x, position.y, position.z, 1.0);
-        }
-      `,
-      fragmentShader: `
-        uniform float u_time;
-
-        varying vec2 vUv;
-
-        float circle(float radius, vec2 uv) {
-            float value = distance(vec2(
-                    uv.x,
-                    // uv.x + 0.025*cos((10.*uv.y + u_time)*4.),
-                    uv.y + 0.025*sin((10.*uv.x + u_time)*4.)
-                ), vec2(0.5));
-            return 1. - step(radius, value);
-        }
-
-        float circle2(float radius, vec2 uv) {
-            float value = distance(vec2(
-                    // uv.x,
-                    uv.x + 0.025*cos((5.*uv.y + u_time)*4.),
-                    uv.y + 0.025*sin((5.*uv.x + u_time)*2.)
-                ), vec2(0.5));
-            return step(radius, value);
-        }
-
-        void main() {
-            // vec2 uv = gl_FragCoord.xy/u_resolution;
-
-            float alpha = 0.9;
-            vec3 white = vec3(1.0);
-            vec3 black = vec3(0.0);
-            vec3 red = vec3(1.0, 0.0, 0.0);
-
-            vec3 color = white;
-
-            float radius = 0.45;
-            color = mix(color, red, 1. - circle(radius, vUv));
-            color = mix(color, black, 1. - circle2(radius - 0.05, vUv));
-
-            gl_FragColor = vec4(color, alpha);
-            // gl_FragColor = vec4(1.0, 0.0, 0.0, alpha);
-        }
-      `,
-    });
+    // without shader
+    const boxMaterial = new THREE.MeshPhongMaterial({ color: 'red' });
+    // with shader
+    // const boxMaterial = new THREE.ShaderMaterial({
+    //   uniforms: this.uniforms,
+    //   vertexShader: `
+    //     uniform float u_time;
+    //     varying vec2 vUv;
+    //     void main() {
+    //       vUv = uv;
+    //       gl_Position = projectionMatrix
+    //         * modelViewMatrix
+    //         * vec4(position.x, position.y, position.z, 1.0);
+    //     }
+    //   `,
+    //   fragmentShader: `
+    //     uniform float u_time;
+    //     varying vec2 vUv;
+    //     float circle(float radius, vec2 uv) {
+    //         float value = distance(vec2(
+    //                 uv.x,
+    //                 // uv.x + 0.025*cos((10.*uv.y + u_time)*4.),
+    //                 uv.y + 0.025*sin((10.*uv.x + u_time)*4.)
+    //             ), vec2(0.5));
+    //         return 1. - step(radius, value);
+    //     }
+    //     float circle2(float radius, vec2 uv) {
+    //         float value = distance(vec2(
+    //                 // uv.x,
+    //                 uv.x + 0.025*cos((5.*uv.y + u_time)*4.),
+    //                 uv.y + 0.025*sin((5.*uv.x + u_time)*2.)
+    //             ), vec2(0.5));
+    //         return step(radius, value);
+    //     }
+    //     void main() {
+    //         // vec2 uv = gl_FragCoord.xy/u_resolution;
+    //         float alpha = 0.9;
+    //         vec3 white = vec3(1.0);
+    //         vec3 black = vec3(0.0);
+    //         vec3 red = vec3(1.0, 0.0, 0.0);
+    //         vec3 color = white;
+    //         float radius = 0.45;
+    //         color = mix(color, red, 1. - circle(radius, vUv));
+    //         color = mix(color, black, 1. - circle2(radius - 0.05, vUv));
+    //         gl_FragColor = vec4(color, alpha);
+    //         // gl_FragColor = vec4(1.0, 0.0, 0.0, alpha);
+    //     }
+    //   `,
+    // });
     const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
     boxMesh.position.x = coord.x;
     boxMesh.position.y = coord.y;
@@ -134,7 +126,6 @@ class Board {
 
   update(t: number) {
     this.uniforms.u_time.value = t / 1000;
-    console.log(this.uniforms);
   }
 
   _initializeBoard() {
