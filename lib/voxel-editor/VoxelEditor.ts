4343a428183586069b9a820e1c661d2429f1855b
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-10-10 11:16:38 -0500

    Begin working on a simple voxel editor.

--- lib/voxel-editor/VoxelEditor.ts
+++ lib/voxel-editor/VoxelEditor.ts
@@ -2,14 +2,26 @@ import * as THREE from 'three';
 
 export class VoxelEditor {
   gg: THREE.Group;
+  base: THREE.Group;
 
   constructor() {
     this.gg = new THREE.Group();
+    this.base = new THREE.Group();
 
-    const boxGeo = new THREE.BoxGeometry();
-    const boxMat = new THREE.MeshStandardMaterial();
-    const boxMesh = new THREE.Mesh(boxGeo, boxMat);
+    this.gg.add(this.base);
 
-    this.gg.add(boxMesh);
+    this.createBase();
+  }
+
+  createBase() {
+    for (let x = -5; x < 5; x++) {
+      for (let y = -5; y < 5; y++) {
+        const boxGeo = new THREE.BoxGeometry();
+        const boxMat = new THREE.MeshNormalMaterial({ wireframe: true });
+        const boxMesh = new THREE.Mesh(boxGeo, boxMat);
+        boxMesh.position.set(x, y, 0);
+        this.base.add(boxMesh);
+      }
+    }
   }
 }
