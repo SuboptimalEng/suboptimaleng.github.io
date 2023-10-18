3b05f6d616ad3d4b5082e6d862af7be882d13975
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-10-18 18:04:44 -0400

    Auto-format glsl files.

--- lib/snakes-and-portals/shaders/portal_fs.glsl
+++ lib/snakes-and-portals/shaders/portal_fs.glsl
@@ -4,20 +4,16 @@ varying vec2 vUv;
 varying vec3 pos;
 
 float circle(float radius, vec2 uv) {
-    float value = distance(vec2(
-            uv.x,
+    float value = distance(vec2(uv.x,
             // uv.x + 0.025*cos((10.*uv.y + u_time)*4.),
-            uv.y + 0.025*sin((10.*uv.x + u_time)*4.)
-        ), vec2(0.5));
+    uv.y + 0.025 * sin((10. * uv.x + u_time) * 4.)), vec2(0.5));
     return 1. - step(radius, value);
 }
 
 float circle2(float radius, vec2 uv) {
     float value = distance(vec2(
             // uv.x,
-            uv.x + 0.025*cos((5.*uv.y + u_time)*4.),
-            uv.y + 0.025*sin((5.*uv.x + u_time)*2.)
-        ), vec2(0.5));
+    uv.x + 0.025 * cos((5. * uv.y + u_time) * 4.), uv.y + 0.025 * sin((5. * uv.x + u_time) * 2.)), vec2(0.5));
     return step(radius, value);
 }
 
@@ -32,8 +28,8 @@ void main() {
     color = mix(color, red, 1. - circle(radius, vUv));
     color = mix(color, black, 1. - circle2(radius - 0.05, vUv));
     if (pos.z > .49 || pos.z < -0.49) {
-      gl_FragColor = vec4(red, alpha);
+        gl_FragColor = vec4(red, alpha);
     } else {
-      gl_FragColor = vec4(color, alpha);
+        gl_FragColor = vec4(color, alpha);
     }
 }
--- lib/snakes-and-portals/shaders/portal_vs.glsl
+++ lib/snakes-and-portals/shaders/portal_vs.glsl
@@ -4,9 +4,7 @@ varying vec2 vUv;
 varying vec3 pos;
 
 void main() {
-    vUv = uv;
-    pos = position;
-    gl_Position = projectionMatrix
-      * modelViewMatrix
-      * vec4(position.x, position.y, position.z, 1.0);
+  vUv = uv;
+  pos = position;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position.x, position.y, position.z, 1.0);
 }
